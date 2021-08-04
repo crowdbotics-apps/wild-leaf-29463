d30ee0a5057c06602ab301f00283db1b13038dfc
Crowdbotics <team@crowdbotics.com>
2021-08-04 17:39:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wild_leaf_29463Identifier",
+  url: "https://wild_leaf_29463Identifier.botics.co",
+  api: "https://wild_leaf_29463Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
